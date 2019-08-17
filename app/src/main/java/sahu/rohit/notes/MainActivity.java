217d6a12b1e0669211799e52972ea330d122cef9
RohitSahu1920 <RohitSahu1920@gmail.com>
2019-08-17 13:06:14 +0530

    notes application

---
+++ app/src/main/java/sahu/rohit/notes/MainActivity.java
@@ -0,0 +1,16 @@
+package sahu.rohit.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mrgames13.jimdo.splashscreen.App.SplashScreenBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
